d630c04ef848067674ab274335ddab9a9c1499e6
Neil Empey <argus.two.2@gmail.com>
2020-05-18 20:57:17 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,10 @@
  class Dog  
     attr_accessor :name, :breed, :age
+    @@all = []
     def initialize(name, breed, age)
       @name = name
       @breed = breed
       @age = age 
-      Dog.new(name, breed, age)
+      self.class.all
     end
   end
